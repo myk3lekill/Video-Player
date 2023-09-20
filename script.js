712f7fa226090c3cbafe4b2078e3d6c58b9436e8
me <me@gmail.com>
2023-09-20 16:20:33 +0200

    sixth commit

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-console.log('javascript is connected')
-// Play & Pause ----------------------------------- //
+//DOM
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen')
 
+
+// Play & Pause ----------------------------------- //
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause', 'fa-play');//show play button instead of pause
+    playBtn.setAttribute('title', 'Play')//show title on button play
+}
+
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');//show pause button instead of play
+        playBtn.setAttribute('title', 'Pause');//show title on button pause
+    } else {
+        video.pause();
+        showPlayIcon();
+    }
+}
+
+// On video End, show play button icon
+video.addEventListener('ended', showPlayIcon);
 
 
 // Progress Bar ---------------------------------- //
@@ -16,3 +45,8 @@ console.log('javascript is connected')
 
 
 // Fullscreen ------------------------------- //
+
+
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click',togglePlay);
