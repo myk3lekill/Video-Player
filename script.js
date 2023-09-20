8a6d2eb4f80fc5f0afed9896fe5e995ea9c07211
me <me@gmail.com>
2023-09-20 16:36:19 +0200

    eighth commit

--- script.js
+++ script.js
@@ -34,10 +34,21 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    //console.log(minutes, seconds);
+    return `${minutes}:${seconds}`;
+}
+
 // Update Progress Bar as the video plays
 function updateProgress() {
     //console.log('current time', video.currentTime, 'duration', video.duration)
-    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`//change css width property in %
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`//Change css width property of progressBar in %
+    currentTime.textContent = `${displayTime(video.currentTime)} /`; //Change the text of the currentTime
+    duration.textContent = `${displayTime(video.duration)}`; //Change the text of the duration
 }
 
 // Volume Controls --------------------------- //
